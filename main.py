7cbb2c4c597e42284edd2fa3b994323a7e15058a
Sean R Reid <sean@torchcodelab.com>
2025-07-25 17:26:34 -0400

    Added CORS, updated POST route

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from sqlmodel import Session, select
 from db import get_session
@@ -7,6 +8,16 @@ from db import get_session
 from models.urls import Urls
 
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.get("/")
@@ -21,6 +32,8 @@ async def get_all_urls(session: Session = Depends(get_session)):
     results = session.exec(statement).all()
     return results
 
+# READ data
+
 
 @app.get("/urls/{id}")
 async def get_single_url(id: str, session: Session = Depends(get_session)):
@@ -29,10 +42,17 @@ async def get_single_url(id: str, session: Session = Depends(get_session)):
 
     return result
 
+# CREATE data
+
 
 @app.post("/urls/add")
-async def add_url(title: str, url: str):
-    return {title: url}
+async def add_url(payload: Urls, session: Session = Depends(get_session)):
+    new_url = Urls(title=payload.title, long_url=payload.long_url,
+                   short_url=payload.short_url, user_id=payload.user_id)
+    session.add(new_url)
+    session.commit()
+    session.refresh(new_url)
+    return {"message": f"Added new url with ID: {new_url.id}"}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='localhost', port=8000, reload=True)
